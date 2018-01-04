b57d7286045658a07c40b3cc4c7b38bbc581a556
justamir <34978698+justamir@users.noreply.github.com>
2018-01-04 20:01:09 +0800

    penambahan javascript

--- aseet/js/style.js
+++ aseet/js/style.js
@@ -1,1 +1,49 @@
 
+
+		$(document).ready(function() {
+				// scroll ke target
+				$('a[href^="#"]').on('click',function (e) {
+					e.preventDefault();
+
+					var target = this.hash,
+					$target = $(target);
+
+					$('html,body').stop().animate({'scrollTop':$target.offset().top},2000,'swing',function(){
+						window.location.hash =target;
+					});
+				});
+				// end scroll
+
+					
+
+				// burger+toggle menu
+				$('.burger').click(function() {
+					$('.burger').toggleClass('active');
+				});
+
+				
+				$('.burger').click(function() {
+					$('.menu1').slideToggle(800);
+				});
+				// end toggle
+
+
+		}); 
+		// end document
+
+		// sticky navbar
+		$(window).on('scroll',function () {
+			if ($(window).scrollTop()) {
+				$('nav').addClass('shrink');
+			}
+			else{
+				$('nav').removeClass('shrink');
+			}
+		});
+		// end sticky
+
+		// back to top
+		function scrolltotop(){
+			$('html,body').animate({scrollTop :0},2000);
+		}
+	
